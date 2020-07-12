f343921876dee92e5ef4251c5bec91e41fa59c8f
Nicola Brisotto <nicolabrisotto@gmail.com>
2020-07-12 21:06:45 +0200

    Import Date-fns from a Pika.dev CDN
    https://www.pika.dev/cdn
    
    * Every npm package can be loaded from Pika CDN as a modern ESM import.
    * smart: Every package is optimized to the environment that requested it. That means that only legacy browsers pay the cost of transpiled/polyfilled code, and modern environments get native code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import * as dateFns from 'https://cdn.pika.dev/date-fns@^2.14.0';
+
 function component() {
     const element = document.createElement('div');
     //the format function is defined by  <script src="https://unpkg.com/browse/date-fns@2.14.0"></script>
-    element.innerHTML = dateFns.format(new Date(2014, 1, 11), 'MM/DD/YYYY')
+    element.innerHTML = dateFns.format(new Date(2014, 1, 11), 'mm/dd/yyyy')
     return element;
 }
 document.body.appendChild(component());
